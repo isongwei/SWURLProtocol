20f796f21aeeb16f55463afe517ba599a74b0a36
Song <isongwei1992@gmail.com>
2017-08-08 13:55:32 +0800

    Initial Commit

---
+++ NSURLProtocol黑魔法/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLProtocol黑魔法
+//
+//  Created by iSongWei on 2017/8/8.
+//  Copyright © 2017年 iSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLProtocol黑魔法/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NSURLProtocol黑魔法
+//
+//  Created by iSongWei on 2017/8/8.
+//  Copyright © 2017年 iSong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
